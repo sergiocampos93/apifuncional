ee31f2ed8495b10178831b1c856e6cb51b9f7c3e
Sérgio Campos <srabelocampos@gmail.com>
2026-03-08 23:26:10 -0300

    feat: implement Product CRUD API with EF Core and initial migration

---
+++ ApiFuncional/Data/ApiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ApiFuncional.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiFuncional.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
---
+++ ApiFuncional/Program.cs
@@ -0,0 +1,34 @@
+using ApiFuncional.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<ApiDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
