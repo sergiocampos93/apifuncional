e90e2e5da3e6be2a8152df62d19a8e4a6127d4a9
Sérgio Campos <srabelocampos@gmail.com>
2026-03-19 20:52:52 -0300

    refactor(config): organize application configuration using extension methods

---
+++ ApiFuncional/Configuration/ApiConfig.cs
@@ -0,0 +1,15 @@
+﻿namespace ApiFuncional.Configuration
+{
+    public static class ApiConfig
+    {
+        public static WebApplicationBuilder AddApiConfig(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers()
+                            .ConfigureApiBehaviorOptions(option =>
+                            {
+                                option.SuppressModelStateInvalidFilter = true;
+                            });
+            return builder;
+        }
+    }
+}
